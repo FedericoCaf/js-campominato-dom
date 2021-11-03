5ba02fd00bc205befcdb1fa65db5d69e17f6573b
FedericoCaf <fedetheshield09@gmail.com>
2021-11-03 14:13:36 +0100

    bombs-ok-in-progress

--- js/script.js
+++ js/script.js
@@ -1,72 +1,90 @@
 
-const buttonPlay = document.getElementById('button-play');
-buttonPlay.addEventListener('click', function(){
-
-   const containerGrid = document.querySelector('.container-grid');
-   const mainTitle = document.getElementById('main-title');
-   containerGrid.innerHTML = '';
-   mainTitle.innerHTML = '';
-  
-  if(detectChange() === 'Easy'){
-
-    for(let i=0; i<100; i++){  
-      const squareEasy = createSquare(containerGrid);
-      squareEasy.addEventListener('click', function(){
-      this.classList.add('clicked');
-     })
-  }
-  }
- 
-  if(detectChange() === 'Hard') {
-    for(let i=0; i<81; i++){
-      const squareHard = createSquareHard(containerGrid);
-      squareHard.addEventListener('click', function(){
-      this.classList.add('clicked');
-     })
-       
-    }
-  }
-  if(detectChange() === 'Crazy') {
-    for(let i=0; i<49; i++){
-      const squareCrazy = createSquareCrazy(containerGrid);
-      squareCrazy.addEventListener('click', function(){
-      this.classList.add('clicked');
-     })
-    
-    }
-  }
-
+document.getElementById('button-play').addEventListener('click', function(){
+  play();
 })
 
+function play(){
+  const level = parseInt(document.getElementById('level').value);
+  console.log(level);
+   const gridLevels = [100, 81, 49]
+   const cellNumbers = gridLevels[level-1];
+   const cellPerRow = Math.sqrt(cellNumbers);
+   console.log('cell per row',cellPerRow);
 
-function createSquare(target){
-  let squareEasy = document.createElement('div');
-  squareEasy.className = 'square easy';
-  target.append(squareEasy);
-  return squareEasy;
-}
-function createSquareHard(target){
-  let squareHard = document.createElement('div');
-  squareHard.className = 'square hard';
-  target.append(squareHard);
-  return squareHard;
-}
-function createSquareCrazy(target){
-  let squareCrazy = document.createElement('div');
-  squareCrazy.className = 'square crazy';
-  target.append(squareCrazy);
-  return squareCrazy;
+   const BOMBS_NUMBER = 16;
+   const bombs = generateBombs();
+  //  console.log('bombe',bombs);
+
+   document.querySelector('main').innerHTML = '';
+
+   console.log('bombe',bombs);
+
+   createSquare();
+   
+   function createSquare(){
+
+    const grid = document.createElement('div');
+    grid.className = 'container-grid';
+    
+    for (let i = 1; i <=cellNumbers; i++) {
+      let square = document.createElement('div');
+      square.className = 'square';
+      square.innerHTML = `<span> ${i} </span>`;
+      const cellSize = `calc(100% / ${cellPerRow})`;
+     
+      square.style.width = cellSize;
+      square.style.height = cellSize;
+
+      square.addEventListener('click', handleClickSquare);
+  
+      grid.append(square);
+      // console.log(square);
+    }
+   document.querySelector('main').append(grid);
+  }
+
+  function handleClickSquare(event){
+    console.log('event', event.target.innerText);
+    const squareClicked = parseInt(event.target.innerText);
+    console.log('square clicked', squareClicked);
+    if(bombs.includes(squareClicked)){
+      this.classList.add('clicked-bomb') 
+
+      for (let i = 1; i <= cellNumbers; i++) {
+        if(bombs.includes(i)){
+          this.classList.add('clicked-bomb');
+        } 
+      }
+    }
+
+   
+    this.classList.add('clicked');
+  }
+
+  function generateBombs(){
+    const bombs = [];
+    console.log('BOMBS NUMBER',BOMBS_NUMBER);
+    
+    while(bombs.length < BOMBS_NUMBER){
+      const bomb = getRandomInt(1, cellNumbers);
+      if (!bombs.includes(bomb)) bombs.push(bomb);
+      
+    }
+    return bombs;
+  }
 }
 
-
-function detectChange() {
-  let opzioneSelect = document.getElementById("difficolta").value;
-  // console.log(opzioneSelect);
-  // console.log('Easy');
-  if(opzioneSelect == 1) return 'Easy';
-  if(opzioneSelect == 2) return 'Hard';
-  if(opzioneSelect == 3) return 'Crazy';
-
+function getRandomInt(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-let opzioneScelta = detectChange();
-console.log(opzioneScelta);
+   
+   
+
+  
+  
+
+
+
+
+
+
